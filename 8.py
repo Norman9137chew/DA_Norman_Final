5083489bb7191ac78f623b86dd3724b2ecf8ff01
norman <chewnorman@gmail.com>
2020-02-27 10:00:21 +0800

    argseasegda

---
+++ 8.py
@@ -0,0 +1,11 @@
+import unittest
+
+class Mytestcase(unittest.TestCase):
+
+    def test_website(self):
+        print("Testing")
+
+
+if __name__ == '__main__':
+    unittest.main()
+print("testing")
